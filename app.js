33f05ea53944b16f8c50dfbb6e278658853c045c
DarthRL <darthrlu@gmail.com>
2023-08-25 13:55:04 -0230

    11.19 test fail

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
@@ -16,6 +18,6 @@ app.get('/version', (req, res) => {
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('server started on port 5000')
+  console.log(`server started on port ${PORT}`)
 })
 

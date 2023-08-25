55764a3b379acb81e83861a9a289266a6b400bfb
DarthRL <darthrlu@gmail.com>
2023-08-25 13:50:30 -0230

    11.19 test fail

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
+//app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
   res.send('ok')
@@ -16,6 +16,6 @@ app.get('/version', (req, res) => {
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('server started on port 5000')
+  console.log(`server started on port ${PORT}`)
 })
 
